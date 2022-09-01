5d3e24351d5ec13ab7753ce45d2f9a281c486b2e
hartytp <thomas.peter.harty@gmail.com>
2022-09-01 09:08:32 +0100

    AnnotationInterfaces: docs

--- ndscan/interfaces/annotations.py
+++ ndscan/interfaces/annotations.py
@@ -1,32 +1,57 @@
 import enum
 import dataclasses
-from typing import Dict
+from typing import Dict, Optional, List
 from . import common, parameters
 
 class AnnotationType(enum.Enum):
+    COMPUTED_CURVE = "computed_curve"
     CUSTOM = "custom"
+    LOCATION = "location"
 
 @dataclasses.dataclass
 class AnnotationInterface(common.Interface):
-#     parameters: Dict[str, parameters.ParamInterface]
-#     coordinates: Dict[str, AnnotationDataSource]
+    """ Annotations add content to result data plots to display fits, highlight points
+    of interest in the dataset, etc.
+    """
+    data: Optional[Dict]# = None
 
     def __post_init__(self):
         self.interface_type = common.InterfaceType.ANNOTATION
 
-#     @staticmethod
-#     def from_dict(data) -> common.Interface:
-#         if not "interface_subtype" in data:
-#             raise ValueError(
-#                 f"Invalid annotation interface schema (missing  annotation subtype): {data}"
-#             )
+    @staticmethod
+    def from_dict(data) -> common.Interface:
+        if not "interface_subtype" in data:
+            raise ValueError(
+                f"Invalid annotation interface description (missing annotation subtype): {data}"
+            )
 
-#         self.parameters = {fqn: parameters.ParameterInterface.decode(desc) for fqn, desc in self.parameters.items()}
+        subtype = AnnotationType(data.pop("interface_subtype"))
 
-#         subtype = AnnotationType(data.pop("interface_subtype"))
+        if subtype == AnnotationType.COMPUTED_CURVE:
+            return ComputedCurveAnnotationInteface(**data)
+        elif subtype == AnnotationType.CUSTOM:
+            return common.CustomInterface.from_dict(data)
+        elif subtype == AnnotationType.LOCATION:
+            return LocationAnnotationInteface(**data)
 
-#         if subtype == ParamType.CUSTOM:
-#             return common.CustomInterface.from_dict(data)
+        raise ValueError(f"Unrecognised annotation subtype: {subtype}")
 
-#         raise ValueError(f"Unrecognised annotation subtype: {subtype}")
+@dataclasses.dataclass
+class ComputedCurveAnnotationInteface(AnnotationInterface):
+    function_name: str
+    associated_channels: str
 
+    def __post_init__(self):
+        super().__post_init__()
+        self.interface_subtype = AnnotationType.COMPUTED_CURVE
+
+
+@dataclasses.dataclass
+class LocationAnnotationInteface(AnnotationInterface):
+    associated_channels: List
+    associated_channels: str
+    coordinates: Optional[Dict]# = None
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.interface_subtype = AnnotationType.LOCATION
